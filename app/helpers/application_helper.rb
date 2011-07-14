e38f6bf811e3304eb9fae3bfd98360b2425b672d
Wouter R. de Vos <wouter.devos@surecreations.com>
2011-07-14 22:28:41 +0200

    added a help page and blueprint css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return a title for a page
+  def title
+    base_title = "Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
